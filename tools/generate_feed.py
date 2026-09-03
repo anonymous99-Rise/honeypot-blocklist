cd16041ed11082bcc40db830fb37a7a06a5dd927
anonymous99-Rise <132993532+anonymous99-Rise@users.noreply.github.com>
2026-09-03 07:35:46 +0000

    Bump version to V1.0.10

--- tools/generate_feed.py
+++ tools/generate_feed.py
@@ -7,7 +7,7 @@ import time
 import sys
 from datetime import datetime, timedelta
 
-__version__ = "V1.0.9"
+__version__ = "V1.0.10"
 
 # ================= 配置区 =================
 HFISH_HOST  = os.environ.get("HFISH_HOST",  "IP:52006")
